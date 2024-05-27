f6f259641c110cfbd944b65dbdf36d90fc17f4b4
Pierre Martinon <pierrecmartinon@gmail.com>
2024-05-27 17:26:44 +0200

    quickfix via US locale

--- GUI_mousetrap.jl
+++ GUI_mousetrap.jl
@@ -1,19 +1,9 @@
 using CTDirect
 using Mousetrap 
-#= conflict with solve :(
-...
-EXIT: Optimal Solution Found.
-ERROR: LoadError: MethodError: no method matching +(::Float64, ::Expr)
-
-the error disappears if we comment using Mousetrap...
-=#
-
-
 
 include("ocp.jl")
-sol = solve(ocp1)
 
-#=
+
 # main window control-toolbox
 main() do app::Application
     window = Window(app)
@@ -22,7 +12,7 @@ main() do app::Application
     button = Button()
     connect_signal_clicked!(button) do self::Button
         println(ocp1)
-        
+        sol = solve(ocp1)
     end
 
     set_child!(window, button)
