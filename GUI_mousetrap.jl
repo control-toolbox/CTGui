86ba96b25d7f340fe4a45a6d81f2e88a2452b8f2
Pierre Martinon <pierrecmartinon@gmail.com>
2024-05-27 11:44:53 +0200

    mousetrap

---
+++ GUI_mousetrap.jl
@@ -0,0 +1,19 @@
+using Mousetrap
+using CTDirect
+
+# main window control-toolbox
+main() do app::Application
+    window = Window(app)
+    set_child!(window, Label("control-toolbox"))
+    present!(window)
+end
+
+# OCP definition
+# load OCP in .jl
+
+
+# solve
+# run and display basic solve
+
+# visualization
+# plot solution
