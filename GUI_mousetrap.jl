550c84b9c27cad880502e820bd3b534b7a080e37
Pierre Martinon <pierrecmartinon@gmail.com>
2024-06-10 12:09:06 +0200

    split GUI-independent actions code

--- GUI_mousetrap.jl
+++ GUI_mousetrap.jl
@@ -3,41 +3,19 @@ using CTBase #for plot
 using Mousetrap
 using JLD2
 
-mutable struct GUI_data
-    
-    current_ocp::Union{Nothing, CTBase.OptimalControlModel}
-    current_sol::Union{Nothing, CTBase.OptimalControlSolution}
-    ocp_path::String
-    ocp_name::String
-    show_ocp_path
+# GUI-agnostic actions
+include("actions.jl")
 
-    function GUI_data()
 
-        data = new()
-        data.current_ocp = nothing
-        data.current_sol = nothing
-        data.ocp_path = "please load problem"
-        data.ocp_name = "ocp"
-        data.show_ocp_path = Label("Current problem: $(data.ocp_path)")
-
-        return data
-    end
-end
-
-# not very useful...
-function display_ocp(ocp)
-    println(ocp)
-end
-
+# Mousetrap actions handlers
 # +++ later ask for ocp name (default: "ocp")
 function on_load_ocp_clicked(self::Button, data::GUI_data)
     file_chooser = FileChooser(FILE_CHOOSER_ACTION_OPEN_FILE)
     on_accept!(file_chooser) do self::FileChooser, file::Vector{FileDescriptor}
         data.ocp_path = get_path(file[1])
-        include(data.ocp_path)
-        set_text!(data.show_ocp_path, "Current problem: $(data.ocp_path)")
-        data.current_ocp = eval(Symbol(data.ocp_name))
-        println("Load problem defined in $(data.ocp_path)")
+        load_ocp(data)
+        set_text!(data.label_show_ocp_path, "Current problem: $(data.ocp_path)")
+        return nothing
     end
     on_cancel!(file_chooser) do self::FileChooser
         println("Load cancelled...")
@@ -46,11 +24,10 @@ function on_load_ocp_clicked(self::Button, data::GUI_data)
     return nothing
 end
 
+
 function on_reload_ocp_clicked(self::Button, data::GUI_data)
-    data.current_ocp = nothing
-    include(data.ocp_path)
-    data.current_ocp = eval(Symbol(data.ocp_name))
-    println("Reload problem defined in $(data.ocp_path)")
+    load_ocp(data)
+    return nothing
 end
 
 # open file in external editor
@@ -67,48 +44,30 @@ function on_edit_ocp_clicked(self::Button, data::GUI_data)
     present!(file_chooser)
     return nothing
 end
+
+
 # output is currently in julia REPL, try to pass textfield ?
 # note: unless we bundle the app we will have the julia repl anyway, so maybe leave the outputs in it ?
 # +++ try to use Logging ?
 function on_solve_clicked(self::Button, data::GUI_data)
-
-    if isnothing(data.current_ocp)
-        println("Please load OCP problem before solving.")
-    else
-        println("Solve problem...")
-        data.current_sol = solve(data.current_ocp)
-    end
+    solve_ocp(data)
     return nothing
 end
 
 # +++ later ask for name, maybe a filechooser ?
 function on_save_sol_clicked(self::Button, data::GUI_data)
-    if isnothing(data.current_sol)
-        println("Please solve problem before saving solution.")
-    else
-        print("Save problem solution...")
-        save_object("solution.jld2", data.current_sol)
-        println(" Done")
-    end
+    save_sol(data)
     return nothing
 end
 
 function on_load_sol_clicked(self::Button, data::GUI_data)
-    print("Load problem solution...")
-    data.current_sol = load_object("solution.jld2")
-    println(" Done")
+    load_sol(data)
     return nothing
 end
 
 # Note: plot appears in standalone window
 function on_plot_clicked(self::Button, data::GUI_data)
-    if isnothing(data.current_sol)
-        println("Please solve problem or load solution before plotting.")
-    else
-        print("Plot solution...")
-        display(plot(data.current_sol))
-        println(" Done")
-    end
+    plot_sol(data)
     return nothing
 end
 
@@ -170,15 +129,12 @@ main() do app::Application
     # +++later add 3 tabs below button bar, cf StackSwitcher ?
 
     # layout blocks: ocp, solve, plot
-    #show_ocp_name ocp_name (editable, bouton edit ?)
-    #ocp_info = hbox(show_ocp_path, show_ocp_name)
-    #ocp_bar = hbox(button_load_ocp, button_reload_ocp, button_edit_ocp)
-    #set_spacing!(ocp_bar, 10)
     ocp_bar = CenterBox(ORIENTATION_HORIZONTAL)
     set_start_child!(ocp_bar, button_load_ocp)
     set_center_child!(ocp_bar, button_reload_ocp)
     set_end_child!(ocp_bar, button_edit_ocp)
-    block_ocp = vbox(data.show_ocp_path, ocp_bar)
+    data.label_show_ocp_path = Label("Current problem: $(data.ocp_path)")
+    block_ocp = vbox(data.label_show_ocp_path, ocp_bar)
     set_spacing!(block_ocp, 3)
     block_solve = hbox(button_solve, button_save_sol)
     block_plot = hbox(button_plot, button_load_sol)
---
+++ actions.jl
@@ -0,0 +1,81 @@
+using CTDirect
+using CTBase #for plot
+using JLD2
+
+# Implement actions independently from GUI framework
+# Ideally could be reused from different GUIs
+
+mutable struct GUI_data
+    
+    current_ocp::Union{Nothing, CTBase.OptimalControlModel}
+    current_sol::Union{Nothing, CTBase.OptimalControlSolution}
+    ocp_path::String
+    ocp_name::String
+    label_show_ocp_path # Label for Mousetrap
+
+    function GUI_data()
+
+        data = new()
+        data.current_ocp = nothing
+        data.current_sol = nothing
+        data.ocp_path = "please load problem"
+        data.ocp_name = "ocp"
+        return data
+    end
+end
+
+# not very useful...
+function display_ocp(ocp)
+    println(ocp)
+end
+
+# +++ later ask for ocp name (default: "ocp")
+function load_ocp(data::GUI_data)
+    data.current_ocp = nothing
+    include(data.ocp_path)
+    data.current_ocp = eval(Symbol(data.ocp_name))
+    println("Load problem defined in $(data.ocp_path)")
+    return nothing
+end
+
+
+function solve_ocp(data::GUI_data)
+    if isnothing(data.current_ocp)
+        println("Please load OCP problem before solving.")
+    else
+        println("Solve problem...")
+        data.current_sol = solve(data.current_ocp)
+    end
+    return nothing
+end
+
+# +++ later ask for name, maybe a filechooser ?
+function save_sol(data::GUI_data)
+    if isnothing(data.current_sol)
+        println("Please solve problem before saving solution.")
+    else
+        print("Save problem solution...")
+        save_object("solution.jld2", data.current_sol)
+        println(" Done")
+    end
+    return nothing
+end
+
+function load_sol(data::GUI_data)
+    print("Load problem solution...")
+    data.current_sol = load_object("solution.jld2")
+    println(" Done")
+    return nothing
+end
+
+# Note: plot appears in standalone window
+function plot_sol(data::GUI_data)
+    if isnothing(data.current_sol)
+        println("Please solve problem or load solution before plotting.")
+    else
+        print("Plot solution...")
+        display(plot(data.current_sol))
+        println(" Done")
+    end
+    return nothing
+end
